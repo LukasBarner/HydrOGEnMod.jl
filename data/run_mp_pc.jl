d45fe8ba547e01dd24fc56156cb607444564b20c
LukasBarner <lukas.barner@tu-berlin.de>
2024-02-15 13:25:36 +0100

    Updates replications

--- data/run_mp_pc.jl
+++ data/run_mp_pc.jl
@@ -5,7 +5,7 @@ using JuMP
 pc_results_path = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "results", "pc")
 
 data_pc = get_HydrOGEnMod_data(
-    data = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_pc"),
+    joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_pc")
 );
 write_csv(joinpath(pc_results_path, "precalculated"), data_pc)
 
@@ -19,7 +19,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
         if c ∉ ["LH2"]
             plot_model_nodes(
                 value.(model_pc[:prices][:, c, "Block 1", s, y]);
-                map_title = "Prices for $c in Season $s $y Perfect Competition\n",
+                map_title = "Prices for $c in Season $s $y Perfect Competition",
                 save_path = joinpath(
                     pc_results_path,
                     "graphics",
@@ -29,7 +29,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
             )
             plot_model_nodes(
                 value.(model_pc[:demand][:, c, "Block 1", s, y]);
-                map_title = "Demand for $c in Season $s $y Perfect Competition\n",
+                map_title = "Demand for $c in Season $s $y Perfect Competition",
                 save_path = joinpath(
                     pc_results_path,
                     "graphics",
@@ -42,14 +42,14 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
 
     plot_model_nodes(
         value.(model_pc[:yearly_demand][:, c, y]);
-        map_title = "Annual Demand for $c in $y Perfect Competition\n",
+        map_title = "Annual Demand for $c in $y Perfect Competition",
         save_path = joinpath(pc_results_path, "graphics", "pc_demand_$(c)_$(y).pdf"),
         rasterize = 1,
     )
 
     plot_model_nodes(
         value.(model_pc[:yearly_production][:, c, y]);
-        map_title = "Annual Production for $c in $y Perfect Competition\n",
+        map_title = "Annual Production for $c in $y Perfect Competition",
         save_path = joinpath(pc_results_path, "graphics", "pc_production_$(c)_$(y).pdf"),
         geojson_property_tag = :MODEL_PRODUCER_5,
         rasterize = 1,
@@ -58,7 +58,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     plot_model_nodes(
         value.(model_pc[:yearly_arc_flows][:, c, y]),
         data_pc.arcs;
-        map_title = "Largest Trade Flows $c $y Perfect Competition\n",
+        map_title = "Largest Trade Flows $c $y Perfect Competition",
         save_path = joinpath(pc_results_path, "graphics", "pc_trade_flows_$(c)_$(y).pdf"),
         colorscheme = :linear_gow_65_90_c35_n256,
         linewidth = 2,
@@ -69,7 +69,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     for i in ["Renewable Hydrogen", "Nuclear Hydrogen"]
         plot_model_nodes(
             value.(model_pc[:yearly_input_procurement][:, i, y]);
-            map_title = "Annual Procurement of $i in $y Perfect Competition\n",
+            map_title = "Annual Procurement of $i in $y Perfect Competition",
             save_path = joinpath(
                 pc_results_path,
                 "graphics",
@@ -81,12 +81,10 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     end
 end
 
-########################################################
-
 mp_results_path = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "results", "mp")
 
 data_mp = get_HydrOGEnMod_data(
-    data = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_mp"),
+    joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_mp")
 );
 
 write_csv(joinpath(mp_results_path, "precalculated"), data_mp)
@@ -99,7 +97,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
         if c ∉ ["LH2"]
             plot_model_nodes(
                 value.(model_mp[:prices][:, c, "Block 1", s, y]);
-                map_title = "Prices for $c in Season $s $y Imperfect Competition\n",
+                map_title = "Prices for $c in Season $s $y Imperfect Competition",
                 save_path = joinpath(
                     mp_results_path,
                     "graphics",
@@ -109,7 +107,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
             )
             plot_model_nodes(
                 value.(model_mp[:demand][:, c, "Block 1", s, y]);
-                map_title = "Demand for $c in Season $s $y Imperfect Competition\n",
+                map_title = "Demand for $c in Season $s $y Imperfect Competition",
                 save_path = joinpath(
                     mp_results_path,
                     "graphics",
@@ -122,14 +120,14 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
 
     plot_model_nodes(
         value.(model_mp[:yearly_demand][:, c, y]);
-        map_title = "Annual Demand for $c in $y Imperfect Competition\n",
+        map_title = "Annual Demand for $c in $y Imperfect Competition",
         save_path = joinpath(mp_results_path, "graphics", "mp_demand_$(c)_$(y).pdf"),
         rasterize = 1,
     )
 
     plot_model_nodes(
         value.(model_mp[:yearly_production][:, c, y]);
-        map_title = "Annual Production for $c in $y Imperfect Competition\n",
+        map_title = "Annual Production for $c in $y Imperfect Competition",
         save_path = joinpath(mp_results_path, "graphics", "mp_production_$(c)_$(y).pdf"),
         geojson_property_tag = :MODEL_PRODUCER_5,
         rasterize = 1,
@@ -138,7 +136,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     plot_model_nodes(
         value.(model_mp[:yearly_arc_flows][:, c, y]),
         data_mp.arcs;
-        map_title = "Largest Trade Flows $c $y Imperfect Competition\n",
+        map_title = "Largest Trade Flows $c $y Imperfect Competition",
         save_path = joinpath(mp_results_path, "graphics", "mp_trade_flows_$(c)_$(y).pdf"),
         colorscheme = :linear_gow_65_90_c35_n256,
         linewidth = 2,
@@ -149,7 +147,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     for i in ["Renewable Hydrogen", "Nuclear Hydrogen"]
         plot_model_nodes(
             value.(model_mp[:yearly_input_procurement][:, i, y]);
-            map_title = "Annual Procurement of $i in $y Imperfect Competition\n",
+            map_title = "Annual Procurement of $i in $y Imperfect Competition",
             save_path = joinpath(
                 mp_results_path,
                 "graphics",
@@ -170,7 +168,7 @@ for y in [2030, 2050]
                 model_mp[:prices][:, c, "Block 1", s, y] .-
                 model_pc[:prices][:, c, "Block 1", s, y]
             ));
-            map_title = "Price Difference against Perfect Competition $s $c $y\n",
+            map_title = "Price Difference against Perfect Competition $s $c $y",
             save_path = joinpath(diff_plots_path, "diff_prices_$(c)_$(s)_$(y).pdf"),
             rasterize = 1,
         )
@@ -182,7 +180,7 @@ for y in [2030, 2050]
                 model_mp[:yearly_input_procurement][:, i, y] .-
                 model_pc[:yearly_input_procurement][:, i, y]
             );
-            map_title = "Difference in Annual Procurement of $i in $y Imperfect Competition\n",
+            map_title = "Difference in Annual Procurement of $i in $y Imperfect Competition",
             save_path = joinpath(diff_plots_path, "diff_procurement_$(i)_$(y).pdf"),
             geojson_property_tag = :MODEL_PRODUCER_5,
             rasterize = 1,
--- docs/src/replication.md
+++ docs/src/replication.md
@@ -10,7 +10,7 @@ using JuMP
 pc_results_path = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "results", "pc")
 
 data_pc = get_HydrOGEnMod_data(
-    data = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_pc"),
+    joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_pc")
 );
 write_csv(joinpath(pc_results_path, "precalculated"), data_pc)
 
@@ -24,7 +24,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
         if c ∉ ["LH2"]
             plot_model_nodes(
                 value.(model_pc[:prices][:, c, "Block 1", s, y]);
-                map_title = "Prices for $c in Season $s $y Perfect Competition\n",
+                map_title = "Prices for $c in Season $s $y Perfect Competition",
                 save_path = joinpath(
                     pc_results_path,
                     "graphics",
@@ -34,7 +34,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
             )
             plot_model_nodes(
                 value.(model_pc[:demand][:, c, "Block 1", s, y]);
-                map_title = "Demand for $c in Season $s $y Perfect Competition\n",
+                map_title = "Demand for $c in Season $s $y Perfect Competition",
                 save_path = joinpath(
                     pc_results_path,
                     "graphics",
@@ -47,14 +47,14 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
 
     plot_model_nodes(
         value.(model_pc[:yearly_demand][:, c, y]);
-        map_title = "Annual Demand for $c in $y Perfect Competition\n",
+        map_title = "Annual Demand for $c in $y Perfect Competition",
         save_path = joinpath(pc_results_path, "graphics", "pc_demand_$(c)_$(y).pdf"),
         rasterize = 1,
     )
 
     plot_model_nodes(
         value.(model_pc[:yearly_production][:, c, y]);
-        map_title = "Annual Production for $c in $y Perfect Competition\n",
+        map_title = "Annual Production for $c in $y Perfect Competition",
         save_path = joinpath(pc_results_path, "graphics", "pc_production_$(c)_$(y).pdf"),
         geojson_property_tag = :MODEL_PRODUCER_5,
         rasterize = 1,
@@ -63,7 +63,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     plot_model_nodes(
         value.(model_pc[:yearly_arc_flows][:, c, y]),
         data_pc.arcs;
-        map_title = "Largest Trade Flows $c $y Perfect Competition\n",
+        map_title = "Largest Trade Flows $c $y Perfect Competition",
         save_path = joinpath(pc_results_path, "graphics", "pc_trade_flows_$(c)_$(y).pdf"),
         colorscheme = :linear_gow_65_90_c35_n256,
         linewidth = 2,
@@ -74,7 +74,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     for i in ["Renewable Hydrogen", "Nuclear Hydrogen"]
         plot_model_nodes(
             value.(model_pc[:yearly_input_procurement][:, i, y]);
-            map_title = "Annual Procurement of $i in $y Perfect Competition\n",
+            map_title = "Annual Procurement of $i in $y Perfect Competition",
             save_path = joinpath(
                 pc_results_path,
                 "graphics",
@@ -89,7 +89,7 @@ end
 mp_results_path = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "results", "mp")
 
 data_mp = get_HydrOGEnMod_data(
-    data = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_mp"),
+    joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_mp")
 );
 
 write_csv(joinpath(mp_results_path, "precalculated"), data_mp)
@@ -102,7 +102,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
         if c ∉ ["LH2"]
             plot_model_nodes(
                 value.(model_mp[:prices][:, c, "Block 1", s, y]);
-                map_title = "Prices for $c in Season $s $y Imperfect Competition\n",
+                map_title = "Prices for $c in Season $s $y Imperfect Competition",
                 save_path = joinpath(
                     mp_results_path,
                     "graphics",
@@ -112,7 +112,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
             )
             plot_model_nodes(
                 value.(model_mp[:demand][:, c, "Block 1", s, y]);
-                map_title = "Demand for $c in Season $s $y Imperfect Competition\n",
+                map_title = "Demand for $c in Season $s $y Imperfect Competition",
                 save_path = joinpath(
                     mp_results_path,
                     "graphics",
@@ -125,14 +125,14 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
 
     plot_model_nodes(
         value.(model_mp[:yearly_demand][:, c, y]);
-        map_title = "Annual Demand for $c in $y Imperfect Competition\n",
+        map_title = "Annual Demand for $c in $y Imperfect Competition",
         save_path = joinpath(mp_results_path, "graphics", "mp_demand_$(c)_$(y).pdf"),
         rasterize = 1,
     )
 
     plot_model_nodes(
         value.(model_mp[:yearly_production][:, c, y]);
-        map_title = "Annual Production for $c in $y Imperfect Competition\n",
+        map_title = "Annual Production for $c in $y Imperfect Competition",
         save_path = joinpath(mp_results_path, "graphics", "mp_production_$(c)_$(y).pdf"),
         geojson_property_tag = :MODEL_PRODUCER_5,
         rasterize = 1,
@@ -141,7 +141,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     plot_model_nodes(
         value.(model_mp[:yearly_arc_flows][:, c, y]),
         data_mp.arcs;
-        map_title = "Largest Trade Flows $c $y Imperfect Competition\n",
+        map_title = "Largest Trade Flows $c $y Imperfect Competition",
         save_path = joinpath(mp_results_path, "graphics", "mp_trade_flows_$(c)_$(y).pdf"),
         colorscheme = :linear_gow_65_90_c35_n256,
         linewidth = 2,
@@ -152,7 +152,7 @@ for y in [2030, 2050], c in ["GH2", "NH3", "LH2"]
     for i in ["Renewable Hydrogen", "Nuclear Hydrogen"]
         plot_model_nodes(
             value.(model_mp[:yearly_input_procurement][:, i, y]);
-            map_title = "Annual Procurement of $i in $y Imperfect Competition\n",
+            map_title = "Annual Procurement of $i in $y Imperfect Competition",
             save_path = joinpath(
                 mp_results_path,
                 "graphics",
@@ -173,7 +173,7 @@ for y in [2030, 2050]
                 model_mp[:prices][:, c, "Block 1", s, y] .-
                 model_pc[:prices][:, c, "Block 1", s, y]
             ));
-            map_title = "Price Difference against Perfect Competition $s $c $y\n",
+            map_title = "Price Difference against Perfect Competition $s $c $y",
             save_path = joinpath(diff_plots_path, "diff_prices_$(c)_$(s)_$(y).pdf"),
             rasterize = 1,
         )
@@ -185,7 +185,7 @@ for y in [2030, 2050]
                 model_mp[:yearly_input_procurement][:, i, y] .-
                 model_pc[:yearly_input_procurement][:, i, y]
             );
-            map_title = "Difference in Annual Procurement of $i in $y Imperfect Competition\n",
+            map_title = "Difference in Annual Procurement of $i in $y Imperfect Competition",
             save_path = joinpath(diff_plots_path, "diff_procurement_$(i)_$(y).pdf"),
             geojson_property_tag = :MODEL_PRODUCER_5,
             rasterize = 1,
