e4afa04c90cd57304d328030cf53f8dd311e9877
LukasBarner <lukas.barner@tu-berlin.de>
2024-07-18 20:13:00 +0200

    update benchmark files

--- data/run_cvx_pc.jl
+++ data/run_cvx_pc.jl
@@ -2,13 +2,9 @@ using HydrOGEnMod
 using Gurobi
 using JuMP
 
-pc_results_path = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "results", "cvx_pc")
-
 data_pc =
     get_HydrOGEnMod_data(joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_pc"));
-write_csv(joinpath(pc_results_path, "precalculated"), data_pc)
 
 model_pc = build_optimization_model(data_pc)
 
 optimize!(model_pc, Gurobi.Optimizer)
-write_csv(pc_results_path, model_pc)
--- data/run_mcp_pc.jl
+++ data/run_mcp_pc.jl
@@ -3,16 +3,12 @@ using JuMP, Complementarity
 
 # Before using the package, a valid license string to the PATH solver needs to be specified. 
 # An official license string is available from https://pages.cs.wisc.edu/~ferris/path/LICENSE
-ENV["PATH_LICENSE_STRING"] = "Copy_license_string_here"
-
-pc_results_path = joinpath(dirname(dirname(pathof(HydrOGEnMod))), "results", "mcp_pc")
+# ENV["PATH_LICENSE_STRING"] = "Copy_license_string_here"
 
 data_pc =
     get_HydrOGEnMod_data(joinpath(dirname(dirname(pathof(HydrOGEnMod))), "data", "data_pc"));
-write_csv(joinpath(pc_results_path, "precalculated"), data_pc)
 
 model_pc = build_complementarity_model(data_pc)
 
-options = (output = "yes", time_limit = 18000)
-solve_mcp(model_pc; options)
-write_csv(pc_results_path, model_pc)
+options = (output = "yes", time_limit = 36000, cumulative_iteration_limit = 10000)
+solve_mcp(model_pc; options...)
